f052ae1734d64abafc3ed5771dd769082d1e0672
Melania Gottardo <64848167+Mel-ania@users.noreply.github.com>
2022-02-20 21:32:14 +0100

    Add files via upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float moveInput;
+    private float inputVertical;
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float jumpForce;
+    [SerializeField]
+    float checkRadius;
+    [SerializeField]
+    float distance;
+
+    private int extraJumps;
+    [SerializeField]
+    int extraJumpsValue;
+
+    private bool facingRight = true;
+    private bool isGrounded;
+    private bool isClimbing;
+
+    private Rigidbody2D rb;
+    [SerializeField]
+    Transform groundCheck;
+    
+    [SerializeField]
+    LayerMask whatIsGround;
+    [SerializeField]
+    LayerMask whatIsLadder;
+
+    private void Start()
+    {
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void FixedUpdate()
+    {
+        // Moving
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        // Facing left or right
+        if(facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+
+        // Jumping
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        // Climbing
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distance, whatIsLadder);
+
+        if(hitInfo.collider != null)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                isClimbing = true;
+            }
+        }
+        else
+        {
+            isClimbing = false;
+        }
+
+        if(isClimbing == true && hitInfo.collider != null)
+        {
+            inputVertical = Input.GetAxis("Vertical");
+            rb.velocity = new Vector2(rb.velocity.x, inputVertical * speed);
+            rb.gravityScale = 0;
+        }
+        else
+        {
+            rb.gravityScale = 1;
+        }
+    }
+
+    void Update()
+    {
+        // Jumps
+
+        if(isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        } else if (Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
---
+++ Assets/Scripts/SceneTransictions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransictions : MonoBehaviour
+{
+    private int nextScene;
+
+    [SerializeField]
+    Animator transitionAnim;
+
+    [SerializeField]
+    GameObject soundEffect;
+
+    private void Start()
+    {
+        nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(LoadScene());
+        Instantiate(soundEffect, transform.position, Quaternion.identity);
+        Destroy(other.gameObject);
+    }
+
+    private IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(0.7f);
+        SceneManager.LoadScene(nextScene);
+    }
+}
